c9b583d1053c25da67535b1bb164035a3cc06f1e
an_owl <an_owl@protonmail.com>
2021-12-29 16:47:21 +1300

    tested coordinates conversion

--- src/lib.rs
+++ src/lib.rs
@@ -32,21 +32,42 @@ pub fn run(st: &uefi::table::SystemTable<uefi::prelude::Boot>) -> uefi::Result<(
     g.draw(0).unwrap().unwrap(); //should be only call to g.draw during Gameplay
     board.draw(&mut g).unwrap().unwrap(); //do not draw board to stored buffers it will waste time //TODO handle this
 
-    let mut l_shape = Tetromino::new((2,3),Tetromino::L_SHAPE,BlockColour::Blue);
+    let mut l_shape = Tetromino::new((3,3),Tetromino::L_SHAPE,BlockColour::Blue);
+    let mut square = Tetromino::new((2,2),Tetromino::SQUARE,BlockColour::Yellow);
     l_shape.location = (3,3);
+    square.location = (5,3);
+    //square.set(&mut board);
 
     l_shape.set(&mut board);
-    board.draw(&mut g).unwrap().unwrap();
-    for _ in 0..10 {
+
+    board.draw(&mut g);
+    st.boot_services().stall(1000000);
+
+    for _ in 0..5 {
+        //info!("moved {} to {:?}",l_shape.safe_ror(&mut board),l_shape.location);
+        info!("moved {} to {:?}",l_shape.legal_move((-1,0),&mut board),l_shape.location);
+        board.draw(&mut g);
         st.boot_services().stall(1000000);
-        l_shape.do_and_update(Tetromino::rotate_right,&mut board, &mut g);
     }
-    for _ in 0..10 {
+    info!("moved {} to {:?}",l_shape.legal_move((2,0),&mut board),l_shape.location);
+    for _ in 0..5 {
+        info!("moved {} to {:?}",l_shape.safe_ror(&mut board),l_shape.location);
+        //info!("moved {} to {:?}",l_shape.legal_move((-1,0),&mut board),l_shape.location);
+        board.draw(&mut g);
         st.boot_services().stall(1000000);
-        l_shape.do_and_update(Tetromino::rotate_left,&mut board, &mut g)
+    }
+        for _ in 0..4 {
+        info!("moved {} to {:?}",l_shape.safe_rol(&mut board),l_shape.location);
+        //info!("moved {} to {:?}",l_shape.legal_move((-1,0),&mut board),l_shape.location);
+        board.draw(&mut g);
+        st.boot_services().stall(1000000);
     }
 
-
-
+    for _ in 0..10 {
+        //info!("moved {} to {:?}",l_shape.safe_ror(&mut board),l_shape.location);
+        info!("moved {} to {:?}",l_shape.legal_move((1,0),&mut board),l_shape.location);
+        board.draw(&mut g);
+        st.boot_services().stall(1000000);
+    }
     Ok(uefi::Status::SUCCESS.into())
 }
